cc3aa551ff2f79bd60679d1f75523c34246bbfb8
vp4655 <tino.100@hotmail.com>
2015-12-13 21:21:05 +0100

    Project initialization v0.8

---
+++ config/config.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Valentino on 12.12.2015..
+ */
+'use strict';
+
+module.exports = {
+    NODE_PORT: 5000,
+    TWITTER_API_KEY: 'xOYHAAlwOXMxMGaiJU6hnwsiU',
+    TWITTER_API_SECRET: 'vnhludRD5tb7UafcbJtik0O03ylz8QYLP5qLk9cNtnCPYQEXAb',
+    TWITTER_ACCESS_TOKEN: '4455385697-287zzHJizTtBjQnsJrtyVNsrHIVCB91uu3M7kEh',
+    TWITTER_ACCESS_SECRET: 'f9JWta1AaNX1QVfvlEUutQ56uBPCW5fH0pw6yybOq9mnr'
+};
